9d993b99c45157e8d054e5fcc6809757158ab92d
Anurag <aianurag09@gmail.com>
2022-03-04 00:37:45 +0000

    Added service routes need to add test cases

--- service/models.py
+++ service/models.py
@@ -6,6 +6,9 @@ All of the models are stored in this module
 import logging
 from flask_sqlalchemy import SQLAlchemy
 
+from flask import Flask
+
+
 logger = logging.getLogger("flask.app")
 
 # Create the SQLAlchemy object to be initialized later in init_db()
@@ -18,7 +21,7 @@ class DataValidationError(Exception):
     pass
 
 
-class YourResourceModel(db.Model):
+class ProductModel(db.Model):
     """
     Class that represents a <your resource model name>
     """
@@ -28,89 +31,99 @@ class YourResourceModel(db.Model):
     # Table Schema
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(63))
-
-    def __repr__(self):
-        return "<YourResourceModel %r id=[%s]>" % (self.name, self.id)
+    category = db.Column(db.String(63), nullable=False)
 
     def create(self):
         """
-        Creates a YourResourceModel to the database
+        Creates a Product to the database
         """
         logger.info("Creating %s", self.name)
         self.id = None  # id must be none to generate next primary key
         db.session.add(self)
         db.session.commit()
 
-    def save(self):
+    def update(self):
         """
-        Updates a YourResourceModel to the database
+        Updates a Product to the database
         """
         logger.info("Saving %s", self.name)
+        if not self.id:
+            raise DataValidationError("Update called with empty ID field")
         db.session.commit()
 
     def delete(self):
-        """ Removes a YourResourceModel from the data store """
+        """Removes a Pet from the data store"""
         logger.info("Deleting %s", self.name)
         db.session.delete(self)
         db.session.commit()
 
-    def serialize(self):
-        """ Serializes a YourResourceModel into a dictionary """
-        return {"id": self.id, "name": self.name}
+    def serialize(self) -> dict:
+        """Serializes a Pet into a dictionary"""
+        return {
+            "id": self.id,
+            "name": self.name,
+            "category": self.category,
+        }
 
-    def deserialize(self, data):
+    def deserialize(self, data: dict):
         """
-        Deserializes a YourResourceModel from a dictionary
-
+        Deserializes a Pet from a dictionary
         Args:
-            data (dict): A dictionary containing the resource data
+            data (dict): A dictionary containing the Pet data
         """
         try:
             self.name = data["name"]
+            self.category = data["category"]
+        except AttributeError as error:
+            raise DataValidationError("Invalid attribute: " + error.args[0])
         except KeyError as error:
-            raise DataValidationError(
-                "Invalid YourResourceModel: missing " + error.args[0]
-            )
+            raise DataValidationError("Invalid pet: missing " + error.args[0])
         except TypeError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: body of request contained bad or no data"
+                "Invalid pet: body of request contained bad or no data " + str(error)
             )
         return self
 
     @classmethod
-    def init_db(cls, app):
-        """ Initializes the database session """
+    def init_db(cls, app: Flask):
+        """Initializes the database session
+        :param app: the Flask app
+        :type data: Flask
+        """
         logger.info("Initializing database")
-        cls.app = app
         # This is where we initialize SQLAlchemy from the Flask app
         db.init_app(app)
         app.app_context().push()
         db.create_all()  # make our sqlalchemy tables
 
     @classmethod
-    def all(cls):
-        """ Returns all of the YourResourceModels in the database """
-        logger.info("Processing all YourResourceModels")
+    def all(cls) -> list:
+        logger.info("Processing all Products")
         return cls.query.all()
 
     @classmethod
-    def find(cls, by_id):
-        """ Finds a YourResourceModel by it's ID """
-        logger.info("Processing lookup for id %s ...", by_id)
-        return cls.query.get(by_id)
+    def find(cls, product_id: int):
+        logger.info("Processing lookup for id %s ...", product_id)
+        return cls.query.get(product_id)
 
     @classmethod
-    def find_or_404(cls, by_id):
-        """ Find a YourResourceModel by it's id """
-        logger.info("Processing lookup or 404 for id %s ...", by_id)
-        return cls.query.get_or_404(by_id)
+    def find_or_404(cls, product_id: int):
+        logger.info("Processing lookup or 404 for id %s ...", product_id)
+        return cls.query.get_or_404(product_id)
 
     @classmethod
-    def find_by_name(cls, name):
-        """Returns all YourResourceModels with the given name
-
-        Args:
-            name (string): the name of the YourResourceModels you want to match
-        """
+    def find_by_name(cls, name: str) -> list:
         logger.info("Processing name query for %s ...", name)
         return cls.query.filter(cls.name == name)
+    
+    @classmethod
+    def find_products_of_same_category(cls, name: str):
+        category = cls.query.filter(cls.name == name).first().category()
+        return cls.query.filter(cls.category == category)
+
+
+    @classmethod
+    def find_by_category(cls, category: str) -> list:
+        logger.info("Processing category query for %s ...", category)
+        return cls.query.filter(cls.category == category)
+
--- service/routes.py
+++ service/routes.py
@@ -4,16 +4,20 @@ My Service
 Describe what your service does here
 """
 
+from itertools import product
+from math import prod
 import os
 import sys
 import logging
 from flask import Flask, jsonify, request, url_for, make_response, abort
 from . import status  # HTTP Status Codes
+from werkzeug.exceptions import NotFound
+
 
 # For this example we'll use SQLAlchemy, a popular ORM that supports a
 # variety of backends including SQLite, MySQL, and PostgreSQL
 from flask_sqlalchemy import SQLAlchemy
-from service.models import YourResourceModel, DataValidationError
+from service.models import ProductModel, DataValidationError
 
 # Import Flask application
 from . import app
@@ -23,19 +27,105 @@ from . import app
 ######################################################################
 @app.route("/")
 def index():
-    """ Root URL response """
+    """Root URL response"""
+    app.logger.info("Request for Root URL")
     return (
-        "Reminder: return some useful information in json format about the service here",
+        jsonify(
+            name="API to get recommendations for a item",
+            version="1.0",
+            paths=url_for("get_similar_products", _external=True),
+        ),
         status.HTTP_200_OK,
     )
+
+@app.route("/recommendations", methods=["GET"])
+def get_similar_products():
+    """Returns all of the recommendation"""
+    app.logger.info("Request for recommendation for category")
+    pets = []
+    category = request.args.get("category")
+    name = request.args.get("name")
+    if category:
+        products = ProductModel.find_by_category(category)
+    elif name:
+        products = ProductModel.find_pets_of_same_category(name)
+    else:
+        products = ProductModel.all()
+
+    results = [pet.serialize() for pet in products]
+    app.logger.info("Returning %d pets", len(results))
+    return make_response(jsonify(results), status.HTTP_200_OK)
+
+
+@app.route("/recommendations/<int:item_id>", methods=["GET"])
+def get_products(item_id):
+    app.logger.info("Request for product with id: %s", item_id)
+    product = ProductModel.find(item_id)
+    if not product:
+        raise NotFound("Product with id '{}' was not found.".format(item_id))
+
+    app.logger.info("Returning product: %s", product.name)
+    return make_response(jsonify(product.serialize()), status.HTTP_200_OK)
+
+
+@app.route("/recommendations", methods=["POST"])
+def create_pets():
+    app.logger.info("Request to create a Product")
+    check_content_type("application/json")
+    product = ProductModel()
+    product.deserialize(request.get_json())
+    product.create()
+    message = product.serialize()
+    location_url = url_for("get_products", item_id=product.id, _external=True)
+
+    app.logger.info("product with ID [%s] created.", product.id)
+    return make_response(
+        jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+    )
+
+
+@app.route("/recommendations/<int:item_id>", methods=["PUT"])
+def update_products(item_id):
+    app.logger.info("Request to update pet with id: %s", item_id)
+    check_content_type("application/json")
+    product = ProductModel.find(item_id)
+    if not product:
+        raise NotFound("product with id '{}' was not found.".format(item_id))
+    product.deserialize(request.get_json())
+    product.id = item_id
+    product.update()
+
+    app.logger.info("product with ID [%s] updated.", product.id)
+    return make_response(jsonify(product.serialize()), status.HTTP_200_OK)
+
+
+@app.route("/recommendations/<int:item_id>", methods=["DELETE"])
+def delete_pets(item_id):
+    app.logger.info("Request to delete pet with id: %s", item_id)
+    product = ProductModel.find(item_id)
+    if product:
+        product.delete()
+
+    app.logger.info("product with ID [%s] delete complete.", item_id)
+    return make_response("", status.HTTP_204_NO_CONTENT)
 
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
 
+def check_content_type(media_type):
+    """Checks that the media type is correct"""
+    content_type = request.headers.get("Content-Type")
+    if content_type and content_type == media_type:
+        return
+    app.logger.error("Invalid Content-Type: %s", content_type)
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        "Content-Type must be {}".format(media_type),
+    )
 
 def init_db():
     """ Initializes the SQLAlchemy app """
     global app
-    YourResourceModel.init_db(app)
+    ProductModel.init_db(app)
