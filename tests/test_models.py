3ad8e414bebeb5c0066f09167a4af9a74b04bb35
freshao1017 <99987102+freshao1017@users.noreply.github.com>
2022-03-05 18:12:00 -0500

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -112,3 +112,12 @@ class TestRecommendationModel(unittest.TestCase):
         self.assertEqual(recommendation.id, 1)
         recommendations = Recommendation.all()
         self.assertEqual(len(recommendations), 1)
+        
+    def test_delete_a_recommendation(self):
+        """Delete a item"""
+        recommendation = Recommendation(id=1, src_product_id=100, rec_product_id=200, type= "UP_SELL")
+        recommendation.create()
+        self.assertEqual(len(recommendation.all()), 1)
+        # delete the  recommendation and make sure it isn't in the database
+        product.delete()
+        self.assertEqual(len( recommendation.all()), 0)    
